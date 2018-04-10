6e401340473a1c12f302eaab1dccf69e9f68e500
AAmberFan <filuwan@gmail.com>
2018-04-10 15:46:54 -0400

    modified sympy

--- sympy-master/sympy/sets/__init__.py
+++
@@ -1,9 +0,0 @@
-from .sets import (Set, Interval, Union, EmptySet, FiniteSet, ProductSet,
-        Intersection, imageset, Complement, SymmetricDifference)
-from .fancysets import ImageSet, Range, ComplexRegion
-from .contains import Contains
-from .conditionset import ConditionSet
-from .ordinals import Ordinal, OmegaPower, ord0
-from ..core.singleton import S
-Reals = S.Reals
-del S
